1dd60a439f8fbedeb47667af734f82532f646280
Duy Ha <havanduy@gmail.com>
2022-09-30 11:25:28 +0700

    divide component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-import { RouterView} from 'vue-router'
+import Header from "./components/header.vue";
+import Slide1 from "./components/slider_one.vue";
+import Footer from "./components/footer.vue";
+import Contact from "./components/contact.vue";
 </script>
   
 <template>
-<RouterView />
+  <section class="side1 parent1">
+    <Header />
+    <Slide1 />
+  </section>
 </template>
 
 <style scoped>
-
 </style>
---
+++ src/components/contact.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="contact pb-[195px]">
+          <div class="title">
+            <h3 class="flex justify-center text-[64px] font-black relative text-[#F7C51E] top-[135px]">LIÊN HỆ</h3>
+            <p class="flex justify-center text-[#3E3E3E] text-[200px] font-black items-center">OEG ESPORTS</p>
+          </div>
+          <div>
+            <div class="flex px-[312px]">
+              <div class="mr-[112px] px-3">
+                <h4 class="fiter text-[40px] font-black">THÔNG TIN CHÚNG TÔI</h4>
+                <nav>
+                  <ul class="ml-5 mt-[20px] px-[10px]">
+                    <li class="flex gap-[5px] items-center py-[10px]">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 9 11" fill="none"
+                        class="mb-[25px]">
+                        <path
+                          d="M4.5 0C2.20997 0 0.353516 1.85644 0.353516 4.14649C0.353516 4.91233 0.503418 5.70333 0.933591 6.27344L4.5 11L8.0664 6.27344C8.45713 5.75561 8.64648 4.84017 8.64648 4.14649C8.64649 1.85644 6.79004 0 4.5 0ZM4.5 2.40155C5.46357 2.40155 6.24493 3.18292 6.24493 4.14648C6.24493 5.11005 5.46357 5.89142 4.5 5.89142C3.53644 5.89142 2.75507 5.11005 2.75507 4.14649C2.75507 3.18292 3.53644 2.40155 4.5 2.40155Z"
+                          fill="white" />
+                      </svg>
+                      Địa chỉ: Số 131 Thái Hà, Phường Trung Liệt, Quận Đống Đa,Thành phố Hà Nội
+                    </li>
+                    <li class="flex gap-[5px] items-center my-[10px]">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                        class="bi bi-telephone-fill text-white" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                          d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                      </svg>
+                      <strong>Phone:</strong> 02466565025
+                    </li>
+                    <li class="flex gap-[5px] items-center my-[10px]">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                        <path
+                          d="M10.7767 2.00009C10.7312 1.99541 10.6854 1.99541 10.64 2.00009H1.30667C1.24685 2.00102 1.18743 2.00998 1.13 2.02676L5.94667 6.82343L10.7767 2.00009Z"
+                          fill="white" />
+                        <path
+                          d="M11.27 2.46338L6.41664 7.29671C6.29174 7.42088 6.12277 7.49057 5.94664 7.49057C5.77052 7.49057 5.60155 7.42088 5.47664 7.29671L0.666644 2.50005C0.651857 2.55439 0.644017 2.61039 0.643311 2.66671V9.33338C0.643311 9.51019 0.713548 9.67976 0.838573 9.80478C0.963597 9.92981 1.13317 10 1.30998 10H10.6433C10.8201 10 10.9897 9.92981 11.1147 9.80478C11.2397 9.67976 11.31 9.51019 11.31 9.33338V2.66671C11.3073 2.59726 11.2938 2.52866 11.27 2.46338ZM1.76664 9.33338H1.30331V8.85671L3.72664 6.45338L4.19664 6.92338L1.76664 9.33338ZM10.6366 9.33338H10.17L7.73998 6.92338L8.20998 6.45338L10.6333 8.85671L10.6366 9.33338Z"
+                          fill="white" />
+                      </svg><strong>Email:</strong> esports@oeg.vn
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+              <div class="contacts h-[747px] w-[732px] relative top-[-142px]">
+                <div class="mx-[73px] my-[54px]">
+                  <h3 class="text-[48px] text-[#F7C51E] font-black text-center mb-[30px]">LIÊN HỆ</h3>
+                  <form action="">
+                    <input type="text" class="input_bg block">
+                    <input type="text" class="input_bg block">
+                    <input type="text" class="input_bg block">
+                    <input type="text" class="input_bg block">
+                    <input type="text" class="input_bg block">
+                    <input type="submit" value="Gửi" class="bg-[#F7C51E] mt-[40px] px-[228px] py-[10px] text-black text-[20px]  rounded-[3px]">
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
