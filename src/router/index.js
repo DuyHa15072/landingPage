d0e9dd7dc5b96d58da1c0af12d7dd0f5a31992a0
Duy Ha <havanduy@gmail.com>
2022-10-06 10:01:42 +0700

    update html

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-       path: "/",
-     
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/ENG',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
     }
   ]
 })
